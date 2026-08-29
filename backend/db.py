570756796cf9b8d1a793db9a58128c18abce722c
Priyanshu Bhagat <priyanshu0152@gmail.com>
2026-08-29 16:58:21 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,12 @@
 import os
 import psycopg2
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_connection():
     return psycopg2.connect(os.getenv("DATABASE_URL"))
+
+def get_cursor(conn):
+    return conn.cursor(cursor_factory=RealDictCursor)
